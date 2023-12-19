aac8ab47027d8f4f117bcd6bdce712080a3a6a77
cavidan-1907 <cavidanvelizade9@gmail.com>
2023-12-19 17:23:37 +0400

    keyboard

---
+++ app.js
@@ -0,0 +1,40 @@
+let keys=document.querySelectorAll(".key");
+let data=[]
+
+keys.forEach(key=>{
+    data.push(key.getAttribute('data-key'))
+})
+
+
+let rand=Math.floor(Math.random()*keys.length);
+
+const randLetter=function(){
+    keys.forEach(key=>{
+        if(key.getAttribute('data-key')==data[rand]){
+            key.classList.add('active')
+        }
+    })
+}
+window.onload=()=>randLetter()
+
+
+window.onkeydown = (e) => {
+    keys.forEach(key => {
+        if (key.classList.contains('active')) {
+            if(e.key.toLowerCase() == key.getAttribute('data-key').toLowerCase()){
+                window.location.reload()
+            }
+        }
+        else{
+            if(e.key.toLowerCase() == key.getAttribute('data-key').toLowerCase()){
+               key.classList.add('false')
+               setInterval(() => {
+                key.classList.remove('false')
+               }, 1000);
+            }
+        }
+    })
+}
+
+
+
